1ad1fab08a6528ba42d0a11294e3e5c6e224824f
Matt Glaman <nmd.matt@gmail.com>
2019-04-26 21:27:28 -0500

    Use dynamic PHP includes for vendor rules.

---
+++ phpstan/phpstan_deprecation_rules.php
@@ -0,0 +1,15 @@
+<?php
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    return [
+        'includes' => [
+            '../vendor/phpstan/phpstan-deprecation-rules/rules.neon',
+        ],
+    ];
+} elseif (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+    return [
+        'includes' => [
+            '../../../vendor/phpstan/phpstan-deprecation-rules/rules.neon',
+        ],
+    ];
+}
---
+++ phpstan/phpstan_drupal_extension.php
@@ -0,0 +1,15 @@
+<?php
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    return [
+        'includes' => [
+            '../vendor/mglaman/phpstan-drupal/extension.neon',
+        ],
+    ];
+} elseif (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+    return [
+        'includes' => [
+            '../../../vendor/mglaman/phpstan-drupal/extension.neon',
+        ],
+    ];
+}
