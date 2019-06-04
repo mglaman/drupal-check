87007679585ab5d4720bf406d000c770ac5864f1
Matt Glaman <nmd.matt@gmail.com>
2019-06-04 16:30:52 -0500

    Put back php config

---
+++ phpstan/phpstan_deprecation_rules.php
@@ -0,0 +1,17 @@
+<?php
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    return [
+        'includes' => [
+            '../vendor/phpstan/phpstan-deprecation-rules/rules.neon',
+        ],
+    ];
+}
+
+if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+    return [
+        'includes' => [
+            '../../../vendor/phpstan/phpstan-deprecation-rules/rules.neon',
+        ],
+    ];
+}
---
+++ phpstan/phpstan_drupal_extension.php
@@ -0,0 +1,17 @@
+<?php
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    return [
+        'includes' => [
+            '../vendor/mglaman/phpstan-drupal/extension.neon',
+        ],
+    ];
+}
+
+if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
+    return [
+        'includes' => [
+            '../../../vendor/mglaman/phpstan-drupal/extension.neon',
+        ],
+    ];
+}
