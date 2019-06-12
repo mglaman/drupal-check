8060b1f75070ad3ce72736236d163cbc8eb98df1
Matt Glaman <nmd.matt@gmail.com>
2019-06-11 21:58:52 -0500

    Revert changes to config files

--- phpstan/phpstan_deprecation_rules.php
+++ phpstan/phpstan_deprecation_rules.php
@@ -9,10 +9,9 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 }
 
 if (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
-    $path = __DIR__ . '/../../../../vendor';
     return [
         'includes' => [
-            $path . '/phpstan/phpstan-deprecation-rules/rules.neon',
+            '../../../vendor/phpstan/phpstan-deprecation-rules/rules.neon',
         ],
     ];
 }
--- phpstan/phpstan_drupal_extension.php
+++ phpstan/phpstan_drupal_extension.php
@@ -9,10 +9,9 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 }
 
 if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
-    $path = __DIR__ . '/../../../../vendor';
     return [
         'includes' => [
-            $path . '/mglaman/phpstan-drupal/extension.neon',
+            '../../../vendor/mglaman/phpstan-drupal/extension.neon',
         ],
     ];
 }
